890bfd332f778a17220e30be3bf150eafa63893d
VictorCher <192.168.127.1@mail.ru>
2019-01-06 16:03:30 +0300

    Процедурный стиль программирования

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,14 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            draw(1,3,'*');
+            draw(2, 4, '#');
             Console.ReadLine();
+        }
+        static void draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
